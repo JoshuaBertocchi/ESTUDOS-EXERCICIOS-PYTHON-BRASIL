7c718a9184ed9bbc6fa3299651f79280e02691e2
Joshua <joshuabertocchi@gmail.com>
2025-12-09 05:32:16 -0300

    09_12_25 att exercise

--- 01_Exercicios_estrutura_serquencial/estrutura_sequencial.py
+++ 01_Exercicios_estrutura_serquencial/estrutura_sequencial.py
@@ -70,6 +70,9 @@ calculo_total_pagamento = valor_por_hora * horas_mes
 
 print(f"Pagamento final: R$ {calculo_total_pagamento:.2f}")
 # %%
+
+#Faça um programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
 def conversao_fahrenheit_para_celsius(valor):
     try:
         if isinstance(valor,(int,float)):
@@ -81,8 +84,11 @@ def conversao_fahrenheit_para_celsius(valor):
         print(f"Digite apenas números inteiros ou Float!")
 temperatura_em_f = float(input("Temperatura em Fahrenheit: "))
 print(f"{conversao_fahrenheit_para_celsius(temperatura_em_f):.2f} f°")
+
+
 # %%
-# Faça um programa que peça 2 números inteiros e um número real. Calcule e mostre:
+#Faça um programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
 def conversao_celsius_para_fahrenheit(valor):
     try:
         if isinstance(valor,(int,float)):
@@ -95,4 +101,40 @@ def conversao_celsius_para_fahrenheit(valor):
 
 temperatura_em_c = float(input("Temperatura em C°: "))
 print(f"{conversao_celsius_para_fahrenheit(temperatura_em_c):.2f} f°")
+
+# %%
+'''Faça um programa que peça 2 números inteiros e um número real. Calcule e mostre:
+- O produto do dobro do primeiro com metade do segundo .
+- A soma do triplo do primeiro com o terceiro.
+- O terceiro elevado ao cubo.'''
+
+def conversao_str_para_float (valor):
+    try:
+        if not isinstance(valor,(str)):
+            print(f" Valor não é um STR!\n Valor atual: {type(valor)}")
+        else:
+            tratamento = valor.strip().replace(",",".").split()
+            uniao = ''.join(tratamento)
+            print(uniao)
+
+            transformado = float(uniao)
+            return(transformado)
+    except ValueError or SyntaxError as E: 
+        print(f"Error: {E}")
+
+teste_numero: int = 1
+teste_str : str = "2,3"
+type(conversao_str_para_float(teste_str))
+
+
+#%%
+numero_01: int = int(input("Informe um valor INT: "))
+numero_02: int = int(input("Informe outro valor INT: "))
+numero_03  = str(input("Informe um número Real: "))
+
+calculo_produto = (numero_01 * 2) * (numero_02/2) 
+calculo_soma = (numero_01 * 3) + numero_03
+calculo_area_cubo = numero_03 ** 3
+
+print(calculo_produto, calculo_soma, calculo_area_cubo)
 # %%
